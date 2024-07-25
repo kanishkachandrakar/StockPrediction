36a29e1b7680e7cfa0fc27fefc350dce3db4770c
kanishkachandrakar <122943743+kanishkachandrakar@users.noreply.github.com>
2024-07-25 09:53:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 import yfinance as yf
-from keras.model import Load_model
+from tensorflow import keras
 import streamlit as st
 
 
